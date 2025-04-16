798c7fc1d3446096cb25baa2b0e5ca6fe81118a8
Jefferson <junior.jefferson@gmail.com>
2025-04-16 11:25:27 -0300

    adicao de novos arquivos

---
+++ src/main/java/br/com/fiap/FiapCPApplication.java
@@ -0,0 +1,11 @@
+package br.com.fiap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiapCPApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FiapCPApplication.class, args);
+    }
+}
